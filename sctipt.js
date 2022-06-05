c497582c32eb9d7bd808878d948d6a55463324c3
Andrei <andrrei17@gmail.com>
2022-06-05 19:21:58 +0300

    Second lesson

--- sctipt.js
+++ sctipt.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('Скльео фильмов Вы уже просмотрели', '');
+const numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели', '');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,14 +10,38 @@ let personalMovieDB = {
     prival: false
 };
 
+for (let i = 0; i < 2; i ++ ) {
+         const a = prompt('Один из последних просмотренных фильмов?', '');
+         const b = prompt('На сколько оцените его?', "");
+        
+         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log ('done');
+         }else {
+             i--;
+             console.log('error');
+         }
+
+        
+    }
+
+    console.log (personalMovieDB);
+
+if (personalMovieDB.count <= 10){
+    console.log ('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <=30){
+    console.log ('Вы классический зритель');
+} else if (personalMovieDB.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', "");
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB.count);
+
+
+
+
+
